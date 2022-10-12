5f1c66f7ed97af913493cfa0053aa52bf3edcce4
David <cruz-azul99@hotmail.com>
2022-10-12 16:47:58 -0500

    Generator expression added

--- iterators.py
+++ iterators.py
@@ -28,9 +28,21 @@ class FiboIter():
 
       return self.aux
 
-if __name__ == "__main__":
-  fibonacci = FiboIter()
+# if __name__ == "__main__":
+#   fibonacci = FiboIter()
   
-  for element in fibonacci:
-    print(element)
-    time.sleep(1)
+#   for element in fibonacci:
+#     print(element)
+#     time.sleep(1)
+
+
+# Generadores: Son funciones que hacen lo mismo que los iteradores, pero en lugar de clases son funciones
+
+# Generator expression: Esta forma de usarlos es similar a las list comprehension, pero a diferencia de estas, es mejor para valores infinitos o valores muy grandes
+my_list = [0,1,4,7,9,10]
+
+my_second_gen = (x*2 for x in my_list)
+print(next(my_second_gen))
+print(next(my_second_gen))
+print(next(my_second_gen))
+print(next(my_second_gen))
